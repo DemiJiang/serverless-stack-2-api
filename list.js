6cd5729f974e782cb330d4e2451c13911759baab
demi.jiang <demi.jiang@myob.com>
2019-08-05 10:56:14 +1000

    First commit

---
+++ list.js
@@ -0,0 +1,23 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+
+export async function main(event, context) {
+   const params = {
+       TableName: "notesapptest",
+
+       KeyConditionExpression: "userId = :userId",
+       ExpressionAttributeValues: {
+           ":userId": event.requestContext.identity.cognitoIdentityId
+       }
+   };
+
+   try {
+       const result = await dynamoDbLib.call("query", params);
+
+       return success(result.Item);
+
+   }catch (e){
+       return failure({status: false});
+   }
+}
